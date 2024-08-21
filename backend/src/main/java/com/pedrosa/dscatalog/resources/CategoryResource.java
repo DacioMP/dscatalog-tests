57b4de1b9b264c76e216087402f46fadc39db809
Matheus Pedrosa <matheuspedrosa.contato@gmail.com>
2024-08-21 17:35:46 -0400

    H2 database

--- backend/src/main/java/com/pedrosa/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/pedrosa/dscatalog/resources/CategoryResource.java
@@ -1,23 +1,27 @@
 package com.pedrosa.dscatalog.resources;
 
 import com.pedrosa.dscatalog.entities.Category;
+import com.pedrosa.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
+    @Autowired
+    CategoryService service;
+
     @GetMapping
     public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L, "Books"));
-        list.add(new Category(2L, "Electronics"));
+
+        List<Category> list = service.findAll();
+
         return ResponseEntity.ok().body(list);
     }
 }
